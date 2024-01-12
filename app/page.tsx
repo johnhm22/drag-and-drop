4bcd278a18d7586e6599e0359fb8490bc655c570
john <johnhowardm@hotmail.com>
2024-01-12 14:12:56 +0000

    Removing borders used to help tailwind config Making tailwind changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import DropZone from './components/DropZone';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col p-24 border border-red-500'>
-      <h1 className='text-cyan-500 text-xl ml-10 border border-blue-600'>
-        Upload files....
-      </h1>
-      <DropZone className='mt-10 p-16 border border-green-400 ml-56' />
+    <main className='flex min-h-screen flex-col p-24'>
+      <h1 className='text-cyan-500 text-xl'>Upload files....</h1>
+      <DropZone className='mt-10 p-16 ml-56 bg-cyan-100 bg-opacity-40' />
     </main>
   );
 }
