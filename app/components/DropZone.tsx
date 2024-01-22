a20a99e895142c81a803b57c0c73b98c5af6d0fa
john <johnhowardm@hotmail.com>
2024-01-22 13:59:11 +0000

    Removing commented out code Updating tailwind settings Adding handleSubmit function

--- app/components/DropZone.tsx
+++ app/components/DropZone.tsx
@@ -27,7 +27,7 @@ const DropZone = ({ className }: { className: string }) => {
   const [rejectedFiles, setRejectedFiles] = useState<RejectedFile[]>([]);
 
   const onDrop = useCallback(
-    (acceptedFiles: File[], fileRejections: File[]) => {
+    (acceptedFiles: File[], fileRejections: RejectedFile[]) => {
       // Do something with the files
       if (acceptedFiles.length) {
         setFiles((prevFiles) => [
@@ -36,10 +36,7 @@ const DropZone = ({ className }: { className: string }) => {
             Object.assign(file, { preview: URL.createObjectURL(file) })
           ),
         ]);
-
-        //   setShareData((prevState) => [...(prevState || []), shareToAdd]);
-
-        // setFiles( (prevFiles) => [...prevFiles, ...acceptedFiles.map((file) => ...file, { preview: URL.createObjectURL(file) } )  ] );
+        console.log('accpetedFiles', acceptedFiles);
       }
       if (fileRejections?.length) {
         setRejectedFiles((prevFiles) => [...prevFiles, ...fileRejections]);
@@ -48,13 +45,7 @@ const DropZone = ({ className }: { className: string }) => {
     []
   );
 
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    // acceptedFiles,
-    // fileRejections,
-  } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: {
       'image/jpeg': ['.jpeg'],
@@ -66,7 +57,7 @@ const DropZone = ({ className }: { className: string }) => {
   const acceptedFiles = files.map((file) => (
     <li
       key={file.name}
-      className='flex items-start w-2/5 place-content-between'
+      className='flex items-start w-2/5 place-content-between mb-5'
     >
       <div className='flex flex-col gap-2 text-sm'>
         {file.name} - {file.size} bytes
@@ -119,8 +110,28 @@ const DropZone = ({ className }: { className: string }) => {
     );
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!files.length) return;
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append('file', file);
+    });
+    formData.append('upload_preset', 'dragdrop');
+
+    const URL = process.env.NEXT_PUBLIC_CLOUDINARY_URL!;
+
+    const data = await fetch(URL, {
+      method: 'POST',
+      body: formData,
+    }).then((res) => res.json());
+
+    console.log('data', data);
+    setFiles([]);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <div {...getRootProps({ className: className })}>
         <input {...getInputProps()} />
         {isDragActive ? (
@@ -128,6 +139,25 @@ const DropZone = ({ className }: { className: string }) => {
         ) : (
           <p>Drag 'n' drop some files here, or click to select files</p>
         )}
+      </div>
+      {/* <p className='font-light italic'>Test section</p>
+      <label htmlFor='fileInput'>Select files</label>
+      <input
+        type='file'
+        id='fileInput'
+        multiple
+        accept='.jpeg, .png, .jpg'
+        onChange={(e) => console.log(e.target.files)}
+      ></input>
+      <p className='font-light italic'>End of test section</p> */}
+      <div className='flex flex-row justify-between my-5'>
+        <h3 className='text-cyan-500 text-xl'>Preview</h3>
+        <button
+          type='submit'
+          className='rounded-md font-light bg-slate-100 text-sm px-4 h-1/2 border border-slate-300'
+        >
+          Upload files to Cloud
+        </button>
       </div>
       <div>
         <div className='flex flex-col'>
@@ -139,7 +169,7 @@ const DropZone = ({ className }: { className: string }) => {
           <ul>{rejectedFileItems}</ul>
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
